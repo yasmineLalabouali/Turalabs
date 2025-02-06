7dfef4990786597dce1e55b628c8db1eedffd510
yasmineLalabouali <37512440+yasmineLalabouali@users.noreply.github.com>
2025-02-06 21:39:50 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,10 @@
-print("hello render")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
